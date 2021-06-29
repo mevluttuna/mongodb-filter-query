6b66fc78d850ef5537f7d9da4418b8dd281e67bb
Mevlüt Tuna <tuna@digital-art.de>
2021-06-29 23:38:08 +0300

    init create

---
+++ mongodb-filter-query.js
@@ -0,0 +1,22 @@
+
+router.post('/get-filtered-data', async (req, res) => {
+    try {
+        // NOTE: Params
+        const { userId, userEventStatus } = req.body
+
+        // NOTE: Filter
+        let users = (await Model.find({ user: mongoose.Types.ObjectId(userId), userEventStatus }))
+
+        // NOTE: Mapped User Data List
+        users = users.map(q => String(q.eventId))
+
+        // NOTE: Filter Data by Users List
+        const response = await OtherModel.find().where('_id').in(users).select()
+
+        // NOTE: Return Response
+        res.send(response);
+    } catch (error) {
+        // NOTE: Return Server Error with Message
+        res.status(500).send(customError(error.message))
+    }
+})
